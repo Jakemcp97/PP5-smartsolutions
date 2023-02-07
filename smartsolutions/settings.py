b12f0245971f910b13f141ce9848f3d7435131f5
Jakemcp97 <jakemcparland@gmail.com>
2023-02-07 15:47:58 +0000

    checkout forms almost finished

--- smartsolutions/settings.py
+++ smartsolutions/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
+    'crispy_forms',
 
     # 'allauth.socialaccount.providers.google',
     # 'allauth.socialaccount.providers.facebook',
@@ -63,6 +64,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'smartsolutions.urls'
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -77,8 +80,13 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+            'builtins':[
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
