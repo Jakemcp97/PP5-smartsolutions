ff50c0507c308e41330e55b6e04a1197bbd6112d
Jakemcp97 <jakemcparland@gmail.com>
2023-03-14 20:32:26 +0000

    added further functionality to product model - stock

--- products/models.py
+++ products/models.py
@@ -20,6 +20,8 @@ class Product(models.Model):
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
+    in_stock = models.BooleanField(default=True)
+    available = models.DecimalField(max_digits=3, decimal_places=0, default=5)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
